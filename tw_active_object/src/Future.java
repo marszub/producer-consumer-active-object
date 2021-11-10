038f3eeef13aed594bbdc2730f40abccf63bbb1c
Marcin Szubert <marszub@gmail.com>
2021-11-10 12:34:19 +0100

    Future implementation

--- tw_active_object/src/Future.java
+++ tw_active_object/src/Future.java
@@ -1,7 +1,21 @@
-public class Future {
-    protected Resource resource;
-    protected Boolean isReady;
-    //get
-    //set
-    //is ready
+public class Future<T extends Resource> {
+    private T resource;
+    private Boolean isReady;
+
+    public Future(){
+        isReady = false;
+    }
+
+    public void Set(T resource){
+        this.resource = resource;
+        isReady = true;
+    }
+
+    public T Get(){
+        return resource;
+    }
+
+    public boolean IsReady(){
+        return isReady;
+    }
 }
