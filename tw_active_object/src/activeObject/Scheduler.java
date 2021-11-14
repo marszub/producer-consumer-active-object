714cf94894d11602c54d4264a91560ecabc41181
Marcin Malcher <malcher@student.agh.edu.pl>
2021-11-14 23:23:07 +0100

    Implemented Scheduler::enqueueue
    Remember to use the lock when dequeueueuing
    No need to use it for the second queue though, since that is only accesed inside Scheduler execution thread

--- tw_active_object/src/activeObject/Scheduler.java
+++ tw_active_object/src/activeObject/Scheduler.java
@@ -2,10 +2,34 @@ package activeObject;
 
 import activeObject.methodRequest.MethodRequest;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+//Todo: Implement everything that's happening it the second thread - calling servant etc.
 public class Scheduler {
-    // TODO: implement Scheduler
+    private final Servant servant;
+
+    private Queue<MethodRequest> callQueue;
+    private Queue<MethodRequest> priorityCallQueue;
+
+    private Lock primaryQueueLock = new ReentrantLock();
+    private Condition queueEmptyCondition = primaryQueueLock.newCondition();
+
+    public Scheduler(Servant servant)
+    {
+        this.servant = servant;
+        this.callQueue = new LinkedList<>();
+        this.priorityCallQueue = new LinkedList<>();
+    }
+
     public void enqueue(MethodRequest request)
     {
-
+        primaryQueueLock.lock();
+        callQueue.offer(request);
+        queueEmptyCondition.signal();
+        primaryQueueLock.unlock();
     }
 }
