0a244fab4868ddbd0ef70813581ea748503ccb23
Gabriel Kępka <gkepka@student.agh.edu.pl>
2021-11-15 22:06:50 +0100

    Scheduler's main loop implementation.

--- tw_active_object/src/activeObject/Scheduler.java
+++ tw_active_object/src/activeObject/Scheduler.java
@@ -9,7 +9,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 //Todo: Implement everything that's happening it the second thread - calling servant etc.
-public class Scheduler {
+public class Scheduler implements Runnable {
     private final Servant servant;
 
     private Queue<MethodRequest> callQueue;
@@ -18,11 +18,16 @@ public class Scheduler {
     private Lock primaryQueueLock = new ReentrantLock();
     private Condition queueEmptyCondition = primaryQueueLock.newCondition();
 
+    Thread schedulerThread;
+
     public Scheduler(Servant servant)
     {
         this.servant = servant;
         this.callQueue = new LinkedList<>();
         this.priorityCallQueue = new LinkedList<>();
+        this.schedulerThread = new Thread(this);
+        this.schedulerThread.setDaemon(true);
+        this.schedulerThread.start();
     }
 
     public void enqueue(MethodRequest request)
@@ -32,4 +37,56 @@ public class Scheduler {
         queueEmptyCondition.signal();
         primaryQueueLock.unlock();
     }
+
+    public void run ()
+    {
+        while (true) {
+            primaryQueueLock.lock();
+            this.dispatch();
+            primaryQueueLock.unlock();
+        }
+    }
+
+    private void tryExecuteRequest(MethodRequest methodRequest) {
+        if (!priorityCallQueue.isEmpty() && priorityCallQueue.peek().getClass() == methodRequest.getClass()) {
+            priorityCallQueue.add(methodRequest);
+            return;
+        }
+        if (methodRequest.guard(servant)) {
+            methodRequest.call(servant);
+        } else {
+            priorityCallQueue.add(methodRequest);
+        }
+    }
+
+    private void waitTillNotEmpty()
+    {
+        try {
+            queueEmptyCondition.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void dispatch()
+    {
+        if (priorityCallQueue.isEmpty()) {
+            if (callQueue.isEmpty()) {
+                waitTillNotEmpty();
+            } else {
+                tryExecuteRequest(callQueue.remove());
+            }
+        } else {
+            if (priorityCallQueue.peek().guard(servant)) {
+                priorityCallQueue.remove().call(servant);
+            } else {
+                if (callQueue.isEmpty()) {
+                    waitTillNotEmpty();
+                } else {
+                    tryExecuteRequest(callQueue.remove());
+                }
+            }
+        }
+    }
+
 }
